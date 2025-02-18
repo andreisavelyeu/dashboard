112304d8e881b44f63c1086b7f8a404090f9bd49
asavelyeu <asavelyeu@acceleratelearning.com>
2025-02-18 08:04:41 +0300

    feat: add login; metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,17 +2,17 @@ import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import {
-  fetchCardData,
-  fetchLatestInvoices,
-  fetchRevenue,
-} from "../../lib/data";
 import { Suspense } from "react";
 import {
   CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default function Page() {
   return (
@@ -21,14 +21,6 @@ export default function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
